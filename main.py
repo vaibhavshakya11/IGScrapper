c862f175b48504cb4fc9671d8712c52561467e72
vaibhavshakya11 <87256483+vaibhavshakya11@users.noreply.github.com>
2024-07-12 21:46:02 +0530

    refined and made main.py more robust

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_exam_year():
     while True:
         try:
             year = input("Enter the year you will give your exams (e.g., 2025): ").strip()
-            if not year.isdigit() or len(year) != 4 or int(year) not in range(2000, datetime.now().year + 10):
+            if not year.isdigit() or len(year) != 4 or int(year) not in range(datetime.now().year, datetime.now().year + 8):
                 raise ValueError("Invalid input. Please enter a valid four-digit year.")
             return int(year)
         except ValueError as ve:
@@ -31,12 +31,18 @@ def extract_years_from_url(url):
 
 def is_valid_syllabus_for_year(url, exam_year):
     year1, year2 = extract_years_from_url(url)
+    
     if year1:
-        if year2:
-            return year1 <= exam_year <= year2
-        else:
-            return exam_year >= year1
+        if year1 == exam_year and year2 is None:
+            return True
+        if year1 < exam_year and year2 is None:
+            return False
+        if year1 <= exam_year and (year2 is None or year2 >= exam_year):
+            return True
+        if year1 > exam_year:
+            return False
     return False
+    
 
 def extract_subject_name(subject_code):
     url_path = subject_dict[subject_code]
@@ -53,13 +59,10 @@ def download_syllabus(subject_url, subject_code, folder="IGCSE Syllabi"):
     if year2:
         year_range = f"{year1}-{year2}"
     else:
-        year_range = f"{year1}-onwards"
+        year_range = f"{year1}"
     
     subject_name = extract_subject_name(subject_code)
-    if "9-1" in subject_name:
-        filename = f"{subject_name} {subject_code} ({year_range}) Syllabus.pdf"
-    else:
-        filename = f"{subject_name} {subject_code} ({year_range}) Syllabus.pdf"
+    filename = f"{subject_name} {subject_code} ({year_range}) Syllabus.pdf"
     
     response = requests.get(subject_url, stream=True)
     if response.status_code == 200:
@@ -77,40 +80,38 @@ def take_code_input():
     while True:
         try:
             code = input("Enter the four-digit course code (e.g., 0625): ").strip()
+            if code == "0000":
+                return None
             if len(code) != 4 or not code.isdigit():
                 raise ValueError("Invalid input. Please enter a four-digit numerical code.")
             if code not in subject_dict:
                 raise KeyError("Course code not found. Please enter a valid course code.")
         except ValueError as ve:
-            print(ve)
+            print(str(ve))
         except KeyError as ke:
-            print(ke)
+            print(str(ke))
         else:
             return code
 
 def main():
+    print("\nWelcome to IGScrapper!")
+    print("Enter 0000 to exit once you're done!\n")
     exam_year = get_exam_year()
-    
     while True:
         subject_code = take_code_input()
+        if subject_code is None: break
         subject_link = f"https://www.cambridgeinternational.org{subject_dict[subject_code]}"
         all_links = extract_all_links(subject_link)
-        
+        syllabus_available = False
         for val in all_links:
             if val is not None and "syllabus.pdf" in val:
                 subject_url = f"https://www.cambridgeinternational.org{val}"
                 if is_valid_syllabus_for_year(subject_url, exam_year):
                     download_syllabus(subject_url, subject_code)
-        
-        while True:
-            terminate = input("Do you want to download more subjects' syllabi? (Y/N): ").strip().upper()
-            if terminate == "Y":
-                break
-            elif terminate == "N":
-                print("\nExiting the program. \nThanks for using IGScrapper. \nRegards, Vaibhav Shakya.\n")
-                return
-            else:
-                print("Invalid input. Please enter 'Y' for Yes or 'N' for No.")
+                    syllabus_available = True
+        if not syllabus_available:
+            print("Syllabus Unavailable.")
+    print("\nExiting the program. \nThanks for using IGScrapper. \nRegards, Vaibhav Shakya.\n")
 
 if __name__ == "__main__":
     main()
